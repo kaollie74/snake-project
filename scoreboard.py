6e320ecd88d95bb91444736e65f1aa000670b99e
Kyle Olson <kaollie74@gmail.com>
2021-11-21 13:45:11 -0600

    able to save high score to file in project

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,7 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.high_score = 0
+        self.high_score = self.get_high_score_from_file()
         self.score = 0
 
         self.color("white")
@@ -24,8 +24,20 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.set_high_score_to_file()
+
         self.score = 0
         self.update_scoreboard()
+
+    def set_high_score_to_file(self):
+        with open("high_score.txt", mode="w") as file:
+            file.write("High Score: " + str(self.high_score))
+
+    def get_high_score_from_file(self):
+        with open("high_score.txt", mode='r') as file:
+            txt = file.read()
+            score = txt[len(txt) - 1]
+            return int(score)
 
     def increase_score(self):
         self.score += 1
